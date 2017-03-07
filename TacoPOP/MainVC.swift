209c5cb6af2f44ef4e4e6f87b01845c1300e3ae6
Sean Perez <sean.perez29@gmail.com>
2017-03-06 20:24:37 -0700

    Add CollectionView delegate and datasource

--- TacoPOP/MainVC.swift
+++ TacoPOP/MainVC.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, DataServiceDelegate {
     
     @IBOutlet weak var headerView: HeaderView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var ds = DataService.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
         headerView.addDropShadow()
     }
+    
+    func deliciousTacoDataLoaded() {
+        print("Delicious Taco Data Loaded!")
+    }
 
+}
+
+extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ds.tacoArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TacoCell", for: indexPath) as! TacoCell
+        let taco = ds.tacoArray[indexPath.row]
+        cell.configureCell(taco)
+        return cell
+    }
+    
 }
 
---
+++ TacoPOP/TacoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TacoCell.swift
+//  TacoPOP
+//
+//  Created by Sean Perez on 3/6/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    var taco: Taco!
+    
+    func configureCell(_ taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinId.rawValue)
+        tacoLabel.text = "\(taco.productName) Taco"
+    }
+
+}
